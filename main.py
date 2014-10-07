d5479be4f872d0ad04f9e02d282641dcccdf187b
Lorand Madai <lorand.madai@uni-tuebingen.de>
2014-10-07 17:27:55 +0200

    Version 0.1.1 object detection based on morphed img with binary black-white threshold filter
    merging was copied, marked as that.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import cv2
 # 2.
 
 
-WAITKEY = 250
+FRAME_WAITTIME = 25
 
 
 # define testvideo
@@ -46,19 +46,6 @@ cv2.moveWindow("contours", 570, 570)
 bg_sub = cv2.BackgroundSubtractorMOG2()
 
 
-############################### copy
-def find_if_close(cnt1,cnt2):
-    row1,row2 = cnt1.shape[0],cnt2.shape[0]
-    for i in xrange(row1):
-        for j in xrange(row2):
-            dist = np.linalg.norm(cnt1[i]-cnt2[j])
-            if abs(dist) < 30:
-                return True
-            elif i==row1-1 and j==row2-1:
-                return False
-
-################################ copy
-
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -66,7 +53,7 @@ while(cap.isOpened()):
         break
 
     # set region of interest ROI
-    roi = frame[80:515, 15:695]
+    roi = frame[80:525, 15:695]
 
     # show original output
     cv2.imshow('file', frame)
@@ -84,10 +71,12 @@ while(cap.isOpened()):
     er_roi_bg_sub = cv2.erode(roi_bg_sub, er_kernel, iterations = 1)
     # dilate img
     di_kernel = np.ones((5,5),np.uint8)
-    di_roi_bg_sub = cv2.dilate(er_roi_bg_sub,di_kernel,iterations = 1)
+    di_roi_bg_sub = cv2.dilate(er_roi_bg_sub,di_kernel,iterations = 4)
+    # thresholding to easy black-white
+    ret, mo_roi_bg_sub = cv2.threshold(di_roi_bg_sub, 127, 255, cv2.THRESH_BINARY)
 
     # set morphed output
-    mo_roi_bg_sub = di_roi_bg_sub
+    # mo_roi_bg_sub = di_roi_bg_sub
 
     # show morphed image
     cv2.imshow("morphed", mo_roi_bg_sub)
@@ -108,12 +97,17 @@ while(cap.isOpened()):
     #########################################################
     # things i don't understand yet
 
+    # getting contours (of the bg-subtracted img)
+    # ret,thresh_img = cv2.threshold(roi_bg_sub,127,255,cv2.THRESH_BINARY)
+    # contour_list, hierarchy = cv2.findContours(thresh_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
     # getting contours (of the morphed img)
-    ret,thresh_img = cv2.threshold(mo_edges,127,255,cv2.THRESH_BINARY)
-    contour_list, hierarchy = cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    ret,thresh_img = cv2.threshold(mo_roi_bg_sub,127,255,cv2.THRESH_BINARY)
+    contour_list, hierarchy = cv2.findContours(thresh_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
 
-
+    # set a threshold for object area. everything below threshold is ignored
+    area_threshold = 700
     if (len(contour_list) > 0 ):
 
         counter = 0
@@ -121,60 +115,26 @@ while(cap.isOpened()):
 
             popped = False
 
-            print "contour nr" + str(counter) + ": area  = " + str(cv2.contourArea(contour_list[counter]))
-            if (cv2.contourArea(contour_list[counter]) < 200):
+            # print "contour nr" + str(counter) + ": area  = " + str(cv2.contourArea(contour_list[counter]))
+            if (cv2.contourArea(contour_list[counter]) < area_threshold):
                 contour_list.pop(counter)
                 popped = True
             if not popped:
+                # print(cv2.contourArea(contour_list[counter]))
                 counter += 1
 
 
 
-
-        ####################### copy
-        if (len(contour_list) > 0 ):
-
-            LENGTH = len(contour_list)
-            status = np.zeros((LENGTH,1))
-
-            for i,cnt1 in enumerate(contour_list):
-                x = i
-                if i != LENGTH-1:
-                    for j,cnt2 in enumerate(contour_list[i+1:]):
-                        x = x+1
-                        dist = find_if_close(cnt1,cnt2)
-                        if dist == True:
-                            val = min(status[i],status[x])
-                            status[x] = status[i] = val
-                        else:
-                            if status[x]==status[i]:
-                                status[x] = i+1
-
-            unified = []
-            maximum = int(status.max())+1
-            for i in xrange(maximum):
-                pos = np.where(status==i)[0]
-                if pos.size != 0:
-                    cont = np.vstack(contour_list[i] for i in pos)
-                    hull = cv2.convexHull(cont)
-                    unified.append(hull)
-
-        print "contour list:" + str(contour_list)
-        cv2.drawContours(roi, contour_list, -1, (0,255,0), 3)
-        cv2.imshow("contours", roi)
-
-        ###################### copy
-
-        # # draw countours to ROI img and show img
-        # print "contour list:" + str(contour_list)
-        # cv2.drawContours(roi, contour_list, -1, (0,255,0), 3)
-        # cv2.imshow("contours", roi)
+    # draw countours to ROI img and show img
+    # print "contour list:" + str(contour_list)
+    cv2.drawContours(roi, contour_list, -1, (0,255,0), 3)
+    cv2.imshow("contours", roi)
 
 
 
 
 
-    if cv2.waitKey(WAITKEY) & 0xFF == 27:
+    if cv2.waitKey(FRAME_WAITTIME) & 0xFF == 27:
         break
 
 cap.release()
