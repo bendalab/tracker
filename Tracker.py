dcfa1225c3f5f6027007ec79fd1051c094d14e26
Jan Grewe <jan.grewe@g-node.org>
2015-01-02 19:48:54 +0100

    new Roi class and stub for data writing class

--- Tracker.py
+++ Tracker.py
@@ -848,10 +848,61 @@ class Tracker():
         #
         # cv2.waitKey(0)
 
+class ROI(object):
+    def __init__(self, x_1, y_1, x_2, y_2):
+        self.__x_1 = x_1
+        self.__y_1 = y_1
+        self.__x_2 = x_2
+        self.__y_2 = y_2
+
+    @property
+    def x1(self):
+        return self.__x_1
+    
+    @x1.setter
+    def x1(self, x):
+        self.__x_1 = x
+
+    @property
+    def y1(self):
+        return self.__y_1
+
+    @y1.setter
+    def y1(self, x):
+        self.__y_1 = x
+    
+    @property
+    def x2(self):
+        return self.__x_2
+
+    @x2.setter
+    def x2(self, x):
+        self.__x_2 = x
+    
+    @property
+    def y2(self):
+        return self.__y_2
+
+    @y2.setter
+    def y2(self, x):
+        self.__y_2 = x
+
+class DataWriter(object):
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def write_ascii():
+        pass
+
+    def write_nix():
+        pass
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='tracking fish in video file')
-    parser.add_argument('path', type=str, help="absolute file path to video including file name and file extension")
-    args = parser.parse_args()
+    #parser = argparse.ArgumentParser(description='tracking fish in video file')
+    #parser.add_argument('path', type=str, help="absolute file path to video including file name and file extension")
+    #args = parser.parse_args()
 
     tr = Tracker()
     tr.set_video_file()
