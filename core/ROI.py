db17fcfe36a1f7a37fa2edd6f7e64479ccc99555
Lorand Madai-Tahy <lorand.madai@uni-tuebingen.de>
2015-05-08 14:05:04 +0200

    added class variable name

--- core/ROI.py
+++ core/ROI.py
@@ -1,9 +1,10 @@
 class ROI(object):
-    def __init__(self, x_1, y_1, x_2, y_2):
-            self.__x_1 = x_1
-            self.__y_1 = y_1
-            self.__x_2 = x_2
-            self.__y_2 = y_2
+    def __init__(self, x_1, y_1, x_2, y_2, name):
+        self._name = name
+        self._x_1 = x_1
+        self._y_1 = y_1
+        self._x_2 = x_2
+        self._y_2 = y_2
 
     def import_cfg_values(self, cfg):
         self.x1 = cfg.getint('roi', 'x1')
@@ -13,33 +14,37 @@ class ROI(object):
 
 
     @property
+    def name(self):
+        return self._name
+
+    @property
     def x1(self):
-        return self.__x_1
+        return self._x_1
 
     @x1.setter
     def x1(self, value):
-        self.__x_1 = value
+        self._x_1 = value
 
     @property
     def y1(self):
-        return self.__y_1
+        return self._y_1
 
     @y1.setter
     def y1(self, value):
-        self.__y_1 = value
+        self._y_1 = value
 
     @property
     def x2(self):
-        return self.__x_2
+        return self._x_2
 
     @x2.setter
     def x2(self, value):
-        self.__x_2 = value
+        self._x_2 = value
 
     @property
     def y2(self):
-        return self.__y_2
+        return self._y_2
 
     @y2.setter
     def y2(self, value):
-        self.__y_2 = value
+        self._y_2 = value
