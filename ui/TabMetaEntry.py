28270324536e7c9046e9839ae5dfc3cec3d60ffd
Lorand Madai-Tahy <lorand.madai@uni-tuebingen.de>
2015-06-08 14:01:35 +0200

    properly shows contents of odml file

--- ui/TabMetaEntry.py
+++ ui/TabMetaEntry.py
@@ -1,8 +1,43 @@
 from PyQt4 import QtGui, QtCore
+import odml
 
-class TabMetaEntry(QtGui.QScrollArea):
-    def __init__(self, name, path):
+
+# maybe enough to inherit from QTreeView
+#class TabMetaEntry(QtGui.QScrollArea):
+class TabMetaEntry(QtGui.QTreeWidget):
+    def __init__(self, path):
         super(TabMetaEntry, self).__init__()
 
-        model = QtCore.QModel
-        tree_view = QtGui.QTreeView()
+        self.parents = []
+        self.setColumnCount(2)
+
+        self.extract_odml_data(path)
+        self.addTopLevelItems(self.parents)
+        self.expandAll()
+        for c in range(self.columnCount()):
+            self.resizeColumnToContents(c)
+
+    def extract_odml_data(self, path):
+        odml_data = odml.tools.xmlparser.load(path)
+        for s in odml_data.sections:
+            tree_item = QtGui.QTreeWidgetItem()
+            tree_item.setText(0, s.name)
+            self.add_subsections_to_entry(tree_item, s)
+            self.parents.append(tree_item)
+
+    def add_subsections_to_entry(self, tree_item, section):
+        if section.sections is not None and len(section.sections) > 0:
+            for s in section.sections:
+                # add subsections recursiveley
+                sub_item = QtGui.QTreeWidgetItem()
+                sub_item.setText(0, s.name)
+                self.add_subsections_to_entry(sub_item, s)
+                tree_item.addChild(sub_item)
+
+            # add values of section
+        if section.properties is not None and len(section.properties) > 0:
+            for p in section.properties:
+                prop_item = QtGui.QTreeWidgetItem()
+                prop_item.setText(0, p.name)
+                prop_item.setData(1, 0, QtCore.QVariant(QtCore.QString(str(p.value))))
+                tree_item.addChild(prop_item)
