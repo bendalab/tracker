139b4752386c4b065d7abcc42fbb4f833bfcc77b
Lorand Madai-Tahy <lorand.madai@uni-tuebingen.de>
2015-06-08 15:30:03 +0200

    added name as class variable

--- ui/TabMeta.py
+++ ui/TabMeta.py
@@ -23,15 +23,33 @@ class TabMeta(QtGui.QTabWidget):
     def __init__(self):
         super(TabMeta, self).__init__()
 
+        self.meta_entry_tabs = []
+
         self.name = "tab_meta"
 
         self.browse_tab = QtGui.QWidget()
-        self.vert_LO_browse_tab = QtGui.QVBoxLayout(self.browse_tab)
-        self.btn_meta_browse = QtGui.QPushButton()
-        self.btn_meta_browse.setObjectName("btn_meta_browse")
-        self.vert_LO_browse_tab.addWidget(self.btn_meta_browse)
 
-        self.addTab(self.browse_tab, "browse templates")
+        self.vert_LO_browse_tab = QtGui.QVBoxLayout(self.browse_tab)
+        self.vert_LO_browse_tab.addItem(QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding))
+        self.ln_edit_browse_template = QtGui.QLineEdit()
+        self.vert_LO_browse_tab.addWidget(self.ln_edit_browse_template)
+        self.ho_LO_buttons = QtGui.QHBoxLayout()
+        self.vert_LO_browse_tab.addLayout(self.ho_LO_buttons)
+        self.btn_template_browse = QtGui.QPushButton()
+        self.btn_template_browse.setObjectName("btn_template_browse")
+        self.ho_LO_buttons.addWidget(self.btn_template_browse)
+        self.btn_template_add = QtGui.QPushButton()
+        self.btn_template_add.setObjectName("btn_template_add")
+        self.ho_LO_buttons.addWidget(self.btn_template_add)
+        self.vert_LO_browse_tab.addItem(QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding))
+        self.ln_edit_remove_template = QtGui.QLineEdit()
+        self.vert_LO_browse_tab.addWidget(self.ln_edit_remove_template)
+        self.btn_template_remove = QtGui.QPushButton()
+        self.btn_template_remove.setObjectName("btn_template_remove")
+        self.vert_LO_browse_tab.addWidget(self.btn_template_remove)
+        self.vert_LO_browse_tab.addItem(QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding))
+
+        self.addTab(self.browse_tab, "templates")
 
         self.setWhatsThis(_fromUtf8(""))
         self.setObjectName(_fromUtf8("tab_meta"))
@@ -44,17 +62,19 @@ class TabMeta(QtGui.QTabWidget):
         self.vert_LO_tab_meta.addItem(QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding))
 
     def add_tab_meta_entry(self, meta_entry):
-        new_tab = TabMetaEntry(meta_entry.name, meta_entry.path)
+        new_tab = TabMetaEntry(meta_entry.path, meta_entry.name)
+        self.meta_entry_tabs.append(new_tab)
         self.addTab(new_tab, meta_entry.name)
 
     # TODO
     def connect_widgets(self, controller):
-        #
+        self.btn_template_browse.clicked.connect(controller.btn_template_browse_clicked)
+        self.btn_template_add.clicked.connect(controller.btn_template_add_clicked)
+        self.btn_template_remove.clicked.connect(controller.btn_template_remove_clicked)
         return
 
     def retranslate_tab_meta(self):
-        # self.lbl_experimenter.setText(_translate(self.name, "Experimenter", None))
-        # self.lbl_fish_id.setText(_translate(self.name, "Fish ID", None))
-        # self.lbl_camera_model.setText(_translate(self.name, "Camera Model", None))
-        # self.lbl_camera_vendor.setText(_translate(self.name, "Camera Vendor", None))
+        self.btn_template_browse.setText(_translate(self.name, "Browse Template", None))
+        self.btn_template_remove.setText(_translate(self.name, "Remove Template", None))
+        self.btn_template_add.setText(_translate(self.name, "Add", None))
         pass
